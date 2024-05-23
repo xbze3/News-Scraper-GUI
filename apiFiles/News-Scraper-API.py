7bb94678b08777884a65da692d536d52a1aadbfb
Ezra Minty <ezranminty@gmail.com>
2024-05-22 20:46:38 -0400

    Move News-Scraper-API.py and tempFetcher.js
    Move News-Scraper-API.py and tempFetcher.js to /apiFiles

---
+++ apiFiles/News-Scraper-API.py
@@ -0,0 +1,72 @@
+from flask import Flask
+from flask_restful import Api, Resource
+import requests
+import re
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+api = Api(app)
+
+class getRequest(Resource):
+
+    def get(self, searchTerm):
+        processedData = self.getNews(searchTerm)
+        return {
+            "searchTerm": searchTerm,
+            "processedData": processedData
+        }
+    
+    def getNews(self, searchTerm):
+
+        bloomberg = self.getBloomberg(searchTerm)
+        investing = self.getInvesting(searchTerm)
+        news = bloomberg + investing
+
+        return news
+    
+    def getBloomberg(self, searchTerm):
+
+        newsArray = []
+
+        userInput = searchTerm.replace(" ", "%20")
+        headers = {'User-Agent': 'Mozilla/5.0'}
+
+        URL = f"https://www.bloomberg.com/search?query={userInput}"
+        page = requests.get(URL, headers=headers)
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        headlineData = [x.get_text() for x in soup.find_all('a', attrs={'class': lambda x: x and re.compile(r'^headline').match(x)})]
+        link = [a['href'] for a in soup.find_all('a', class_= lambda x: x and re.compile(r'^headline').match(x), href=True) if a.text]
+
+        for i in range(0, len(link)):
+            case = {"Headline": headlineData[i], "Link": link[i]}
+            newsArray.append(case)
+
+
+
+        return newsArray
+    
+    def getInvesting(self, searchTerm):
+
+            newsArray = []
+
+            userInput = searchTerm.replace(" ", "%20")
+            headers = {'User-Agent': 'Mozilla/5.0'}
+
+            URL = f"https://www.investing.com/search/?q={userInput}&tab=news"
+            page = requests.get(URL, headers=headers)
+            soup = BeautifulSoup(page.content, "html.parser")
+
+            headlineData = [x.get_text() for x in soup.find_all('a', attrs={'class': 'title'})]
+            link = [a['href'] for a in soup.find_all('a', class_= 'title', href=True) if a.text]
+
+            for i in range(0, (len(headlineData) - 3)):
+                case = {"Headline": headlineData[i], "Link": "https://www.investing.com"+link[i]}
+                newsArray.append(case)
+
+            return newsArray
+
+api.add_resource(getRequest, "/getNews/<string:searchTerm>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
